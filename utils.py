14dc1d3b354da67a3b3a1e362d1a52ac55b4b6b9
Mert Aksoy <mert.aksoy@codesquare.at>
2021-02-16 18:33:59 +0300

    Displaying images in stack

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def extract_encoded_faces(persons):
@@ -8,6 +9,43 @@ def extract_encoded_faces(persons):
     return encoded_faces
 
 
+def stack_images(scale, img_array):
+    rows = len(img_array)
+    cols = len(img_array[0])
+    rows_available = isinstance(img_array[0], list)
+    width = img_array[0][0].shape[1]
+    height = img_array[0][0].shape[0]
+    if rows_available:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if img_array[x][y].shape[:2] == img_array[0][0].shape[:2]:
+                    img_array[x][y] = cv2.resize(img_array[x][y], (0, 0), None, scale, scale)
+                else:
+                    img_array[x][y] = cv2.resize(img_array[x][y], (img_array[0][0].shape[1], img_array[0][0].shape[0]),
+                                                 None, scale, scale)
+                if len(img_array[x][y].shape) == 2:
+                    img_array[x][y] = cv2.cvtColor(img_array[x][y], cv2.COLOR_GRAY2BGR)
+        image_blank = np.zeros((height, width, 3), np.uint8)
+        hor = [image_blank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(img_array[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if img_array[x].shape[:2] == img_array[0].shape[:2]:
+                img_array[x] = cv2.resize(img_array[x], (0, 0), None, scale, scale)
+            else:
+                img_array[x] = cv2.resize(img_array[x], (img_array[0].shape[1], img_array[0].shape[0]), None, scale, scale)
+            if len(img_array[x].shape) == 2:
+                img_array[x] = cv2.cvtColor(img_array[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(img_array)
+        ver = hor
+    return ver
+
+
 def show_images(persons):
-    for index in range(len(persons)):
-        cv2.imshow("Person " + str(index), persons[index].image)
+    images = []
+    for person in persons:
+        images.append(person.image)
+    img_stack = stack_images(0.5, images)
+    cv2.imshow("Persons", img_stack)
