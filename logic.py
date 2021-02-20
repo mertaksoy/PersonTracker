b9e4488fa844723ef55d6e9c886dfc03ca84840b
Mert Aksoy <mert.aksoy@codesquare.at>
2021-02-20 14:48:08 +0300

    Moved face detection logic into PersonTracker class

---
+++ logic.py
@@ -0,0 +1,51 @@
+import face_recognition
+from itertools import compress
+import numpy as np
+from datetime import datetime
+from person import Person
+import utils
+
+
+class PersonTracker:
+
+    def __init__(self, persons):
+        self.persons = persons
+
+    def track_persons(self, scaled_image):
+        face_locations, face_encodings = self.__detect_faces(scaled_image)
+        for faceEncoding, faceLocation in zip(face_encodings, face_locations):
+            y1, x2, y2, x1 = faceLocation
+            if len(self.persons) == 0 and len(face_encodings) > 0:
+                self.persons.append(Person(datetime.now(), None, faceEncoding, scaled_image[y1:y2, x1:x2]))
+            elif len(face_encodings) > 0:
+                matches, match_index = self.__recognize_face(utils.extract_encoded_faces(self.persons), faceEncoding)
+                if matches[match_index]:
+                    self.persons[match_index].last_seen = datetime.now()
+                else:
+                    self.persons.append(Person(datetime.now(), None, faceEncoding, scaled_image[y1:y2, x1:x2]))
+                self.__remove_similar_faces(matches)
+
+    def __detect_faces(self, scaled_image):
+        face_locations = face_recognition.face_locations(scaled_image)
+        face_encodings = face_recognition.face_encodings(scaled_image, face_locations)
+        return face_locations, face_encodings
+
+    def __compare_faces(self, known_face_encodings, face_encoding):
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        face_dis = face_recognition.face_distance(known_face_encodings, face_encoding)
+        return matches, face_dis
+
+    def __recognize_face(self, known_face_encodings, face_encoding):
+        matches, face_dis = self.__compare_faces(known_face_encodings, face_encoding)
+        return matches, np.argmin(face_dis)
+
+    def __remove_similar_faces(self, face_matches):
+        index_of_similar_faces = list(compress(range(len(face_matches)), face_matches))
+        n_of_similar_faces = len(index_of_similar_faces)
+        if n_of_similar_faces > 1:
+            position_to_remove = index_of_similar_faces[n_of_similar_faces - 1]
+            self.persons.remove(self.persons[position_to_remove])
+            face_matches.remove(face_matches[position_to_remove])
+            self.__remove_similar_faces(face_matches)
+        else:
+            return
--- main.py
+++ main.py
@@ -1,54 +1,17 @@
 from camera import Camera
-from person import Person
+from logic import PersonTracker
 import cv2
-import face_recognition
-from datetime import datetime
-import numpy as np
 import utils
-from itertools import compress
-
-cam = Camera(640, 480)
-persons = []
-
-
-def remove_similar_faces(face_matches):
-    index_of_similar_faces = list(compress(range(len(face_matches)), face_matches))
-    n_of_similar_faces = len(index_of_similar_faces)
-    if n_of_similar_faces > 1:
-        position_to_remove = index_of_similar_faces[n_of_similar_faces - 1]
-        persons.remove(persons[position_to_remove])
-        face_matches.remove(face_matches[position_to_remove])
-        remove_similar_faces(face_matches)
-    else:
-        return
-
 
 if __name__ == '__main__':
+    persons = []
+    cam = Camera(640, 480)
+    personTracker = PersonTracker(persons)
 
     while True:
         image = cam.read(display=True)
-        scaledImage = Camera.scale_down(image)
-
-        faceLocations = face_recognition.face_locations(scaledImage)
-        faceEncodings = face_recognition.face_encodings(scaledImage, faceLocations)
-
-        for faceEncoding, faceLocation in zip(faceEncodings, faceLocations):
-            y1, x2, y2, x1 = faceLocation
-            if len(persons) == 0 and len(faceEncodings) > 0:
-                persons.append(Person(datetime.now(), None, faceEncoding, scaledImage[y1:y2, x1:x2]))
-            elif len(faceEncodings) > 0:
-                knownFaceEncodings = utils.extract_encoded_faces(persons)
-                matches = face_recognition.compare_faces(knownFaceEncodings, faceEncoding)
-                faceDis = face_recognition.face_distance(knownFaceEncodings, faceEncoding)
-
-                matchIndex = np.argmin(faceDis)
-                if matches[matchIndex]:
-                    persons[matchIndex].last_seen = datetime.now()
-                else:
-                    persons.append(Person(datetime.now(), None, faceEncoding, scaledImage[y1:y2, x1:x2]))
-                remove_similar_faces(matches)
-
-        print('persons: ' + str(len(persons)))
+        personTracker.track_persons(Camera.scale_down(image))
+        print('persons:', len(persons))
         utils.show_images(persons)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
